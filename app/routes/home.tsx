73359f3ce06a21859bd5b507a2d55851b42571f8
Vinciarya <kosliaaryan@gmail.com>
2025-08-06 15:28:45 +0530

    New changes done Improved css

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import { usePuterStore } from "~/lib/puter";
+import { Link, useNavigate } from "react-router";
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 import ResumeCard from "~/components/ResumeCard";
@@ -11,9 +14,16 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!auth.isAuthenticated) navigate("/auth?next=/");
+  }, [auth.isAuthenticated]);
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
+
       <section className="main-section">
         <div className="page-heading py-16">
           <h1>Track Your Application & Resume Ratings</h1>
