a476acd8beb41cfa5c23860be7f6b5a6c07a2a3e
Vinciarya <kosliaaryan@gmail.com>
2025-09-02 21:07:31 +0530

    new changes

--- app/root.tsx
+++ app/root.tsx
@@ -9,8 +9,8 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
-import { usePuterStore } from "./lib/puter";
-import { useEffect } from "react";
+import {usePuterStore} from "~/lib/puter";
+import {useEffect} from "react";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -27,8 +27,9 @@ export const links: Route.LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   const { init } = usePuterStore();
+
   useEffect(() => {
-    init();
+    init()
   }, [init]);
 
   return (
@@ -41,7 +42,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         <script src="https://js.puter.com/v2/"></script>
-
         {children}
         <ScrollRestoration />
         <Scripts />
